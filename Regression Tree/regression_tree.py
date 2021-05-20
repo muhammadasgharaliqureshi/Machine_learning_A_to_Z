fb3d3bdde7d035390ac6123b5f3882f65211e9ae
Muhammad Asghar Ali Qureshi <64986244+muhammadasgharaliqureshi@users.noreply.github.com>
2021-05-20 21:44:30 +0500

    Update regression_tree.py

--- Regression Tree/regression_tree.py
+++ Regression Tree/regression_tree.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed May 19 14:49:42 2021
-
+Regression trees
 @author: Qureshi
 """
 
+import pandas as pd
+
+
+data_set = pd.read_csv('Position_Salaries.csv')
